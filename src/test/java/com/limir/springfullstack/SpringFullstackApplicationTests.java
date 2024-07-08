32293b0af5d4e90cda467bdb3f33712d8d813d9b
Limir <vladimir.puchynski@gmail.com>
2024-07-09 01:34:08 +0300

    first commit

---
+++ src/test/java/com/limir/springfullstack/SpringFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limir.springfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
